aa9059f4d71fdb4f5a7cbc9f30d0170e4b53d54d
MayankSikkalwar <mayanksikkalwar576@gmail.com>
2025-08-15 13:20:31 +0530

    fix-design issue

--- frontend/ai-interview-prep-app/src/pages/LandingPage.jsx
+++ frontend/ai-interview-prep-app/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import HERO_IMG from "../assets/hero-img.png";
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from "react-router-dom";
+import {LuSparkles} from 'react-icons/lu'
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -13,7 +14,7 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="w-full min-h-full h-screen bg-[#FCFBFc]">
+      <div className="w-full min-h-full  bg-[#FFFCEF]">
         <div className="w-[500px] h-[500px] bg-ember-200/20 blur-[65px] absolute top-0 left-0" />
 
         <div className="container mx-auto px-4 pt-6 pb-[200px] relative z-10">
@@ -23,7 +24,7 @@ const LandingPage = () => {
               Interview Prep AI
             </div>
             <button
-              className="bg-gradient-to-r from-[#FF9324] to-[#e99a4b] text -sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
+              className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text -sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
               onClick={() => setOpenAuthModal(true)}
             >
               Login/ Sign Up
@@ -35,36 +36,39 @@ const LandingPage = () => {
             <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
               <div className="flex items-center justify-left mb-2">
                 <div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
-                  AI Powered
+                  <LuSparkles />AI Powered
                 </div>
               </div>
+
+              <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
+                Ace Interviews with <br />
+                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                  AI-Powered
+                </span>{" "}
+                Learning
+              </h1>
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
+                Get role-specific questions, expand answers when you need them,
+                dive deeper into concepts, and organize everything your way.
+                From preparation to mastery - your ultimate interview toolkit is
+                here.
+              </p>
+              
+              <button
+                className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer"
+                onClick={handleCTA}
+              >
+                Get started
+              </button>
             </div>
           </div>
-          <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
-            Ace Interviews with <br />
-            <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
-              AI-Powered
-            </span>{" "}
-            Learning
-          </h1>
         </div>
-
-        <div className="w-full md:w-1/2">
-          <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
-            Get role-specific questions, expand answers when you need them, dive
-            deeper into concepts, and organize everything your way. From
-            preparation to mastery - your ultimate interview toolkit is here.
-          </p>
-        </div>
-        <button
-          className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer"
-          onClick={handleCTA}
-        >
-          Get started
-        </button>
       </div>
     </>
   );
 };
 
-export default LandingPage
+export default LandingPage;
