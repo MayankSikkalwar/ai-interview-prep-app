fbcb3c7e2262819d72603e0d430b697cbcba0104
MayankSikkalwar <mayanksikkalwar576@gmail.com>
2025-08-17 13:34:17 +0530

    added Signup Form

--- frontend/ai-interview-prep-app/src/pages/Auth/SignUp.jsx
+++ frontend/ai-interview-prep-app/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from 'react';
+import  { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 
-const SignUp = () => {
+const SignUp = ({setCurrentPage}) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle Signup Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create an account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-5">Join us today by entering your details below</p>
+
+      <form onSubmit={handleSignUp}>
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+          value={fullName}
+          onChange={({ target }) => setFullName(target.value)}
+          label="Full Name"
+          placeholder="John"
+          type="text"
+          />
+          
+          
+          
+          
+          <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+
+          
+          />
+          <Input
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+          label="password"
+          placeholder="Min 8 characters"
+          type="password"
+          />
+
+        </div>
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button type="submit" className="btn-primary">
+      SIGN UP
+    </button>
+
+    <p className="text-[14px] text-slate-800 mt-3">
+      Already an account?{" "}
+      <button
+        className="font-medium text-primary underline cursor-pointer"
+        onClick={() => {
+          setCurrentPage("login");
+        }}
+      >
+        Login
+      </button>
+    </p>
+      </form>
+    </div>
+  );
+};
 
 export default SignUp
