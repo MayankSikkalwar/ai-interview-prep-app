437b0554435740cafc7f6aa5d13819662a72ad03
MayankSikkalwar <mayanksikkalwar576@gmail.com>
2025-08-19 11:08:16 +0530

    add validation feat

--- frontend/ai-interview-prep-app/src/pages/Auth/Login.jsx
+++ frontend/ai-interview-prep-app/src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,27 @@ const Login = ({ setCurrentPage }) => {
   // handle login form submit
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address");
+      return;
+    }
+    if (!password){
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+    // Login API call
+
+    try {
+    }catch (error){
+      if(error.response && error.response.data.message){
+        setError(error.response.data.message);
+      }else{
+        setError("Something went wrong. Please try again .");
+      }
+    }
   };
 
   return (
